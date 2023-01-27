66a95cf995b6ae5088e9fbae7b6a630298626e63
GriffinMiller21 <Griffin.Miller@dcmail.ca>
2023-01-27 16:44:24 -0500

    *

---
+++ src/ICE1/ICE1.java
@@ -0,0 +1,21 @@
+package ICE1;
+
+import java.util.*;
+
+public class ICE1 {
+	private static Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double user_input;
+		
+		System.out.println("Welcome! My name is Griffin Miller and my banner id is 100715751");
+		System.out.println("Run you fools! - Gandalf, Lord of the rings");
+		System.out.println("Please enter any number: ");
+		user_input = input.nextDouble();
+		
+		System.out.println("Is this your number: " + user_input);
+		
+	}
+
+}
